8c88af7cc52e89abfde14c382bb2beb5234e0561
SatyabratKumarSingh <satyabrat.kumar.singh@hotmail.com>
2016-12-04 01:53:36 +0000

    Fixing saving in cassandra...

--- src/main/scala/cassandrarw/CassandraHelper.scala
+++ src/main/scala/cassandrarw/CassandraHelper.scala
@@ -1,4 +1,5 @@
 package cassandrarw
+
 import models.Tweet
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
@@ -9,13 +10,10 @@ import configreader.CassandraConfig
   */
 object CassandraHelper {
 
-
-  def saveTweetInCassandra(tweet:Tweet): Unit = {
+  def saveTweetInCassandra(tweet: Tweet): Unit = {
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", CassandraConfig.hostName)
     val sc = new SparkContext("local[2]", "example", conf)
-    val rdd = sc.cassandraTable(CassandraConfig.keySpace, CassandraConfig.table).select("employee_id","first_name").as((_:Int,_:String))
-    //val col = sc.parallelize(Seq(Tweet("UK",12,"hardeep.mahajan@gmail.com","Hardeep","K","Mahajan")));
-    // println(col.collect().foreach(println))
-    //col.saveToCassandra(keyspace, table)
+    val col = sc.parallelize(Seq(tweet));
+    col.saveToCassandra(CassandraConfig.keySpace, CassandraConfig.table)
   }
 }
---
+++ src/main/scala/kafkaconsumer/TwitterStreamingConsumer.scala
@@ -0,0 +1,8 @@
+package kafkaconsumer
+
+/**
+  * Created by Satya on 04/12/2016.
+  */
+class TwitterStreamingConsumer {
+
+}
